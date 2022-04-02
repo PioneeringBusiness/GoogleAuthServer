08b3defd0ae9e1c76f0d8dcc1205f474d36c33ef
Charles Yost <charles@cysgourmet.com>
2022-04-02 13:43:45 -0600

    Delete User

--- src/main/kotlin/com/pbussolutions/domain/model/Endpoint.kt
+++ src/main/kotlin/com/pbussolutions/domain/model/Endpoint.kt
@@ -5,7 +5,7 @@ sealed class Endpoint(val path: String) {
     object TokenVerification: Endpoint(path = "/token_verification")
     object GetUserInfo: Endpoint(path = "/get_user")
     object UpdateUserInfo: Endpoint(path = "/update_user")
-    object DeleteUserInfo: Endpoint(path = "/delete_user")
+    object DeleteUser: Endpoint(path = "/delete_user")
     object SignOut: Endpoint(path = "/sign_out")
     object Unauthorized: Endpoint(path = "/unauthorized")
     object Authorized: Endpoint(path = "/authorized")
